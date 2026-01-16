741fafbdb700cef5df43d04badfb0cf17f93bb15
Klara <klara.vandenburg@yahoo.com>
2026-01-16 13:32:21 +0800

    Add health check endpoint

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,19 +1,37 @@
-
 """
 URL configuration for project_name project.
 
 The `urlpatterns` list routes URLs to views.
 Docs: https://docs.djangoproject.com/en/5.1/topics/http/urls/
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from views import health
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('src.login.urls')),  # include your app's router & JWT endpoints
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path("admin/", admin.site.urls),
+    path(
+        "api/", include("src.login.urls")
+    ),  # include your app's router & JWT endpoints
+    path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path("health/", health),
 ]
