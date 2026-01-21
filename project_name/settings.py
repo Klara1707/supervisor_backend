0e793826a55a32faf75718c25a127e167921ad52
Klara <klara.vandenburg@yahoo.com>
2026-01-21 11:33:14 +0800

    Switch to Postgres, add psycopg2-binary

--- project_name/settings.py
+++ project_name/settings.py
@@ -130,14 +130,7 @@ WSGI_APPLICATION = "project_name.wsgi.application"
 
 import dj_database_url
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get(
-            "DATABASE_URL", f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
-        ),
-        conn_max_age=600,
-    )
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 
 # Password validation
