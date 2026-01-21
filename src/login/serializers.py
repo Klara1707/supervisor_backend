d9cd62d7096d19514319ebb3fdae7611cb8e5dcc
Klara <klara.vandenburg@yahoo.com>
2026-01-21 10:25:05 +0800

    serializer update

--- src/login/serializers.py
+++ src/login/serializers.py
@@ -16,3 +16,22 @@ class UserTrainingProgressSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserTrainingProgress
         fields = ["progress_by_popup"]
+
+
+# Registration serializer for RegisterView
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password", "first_name", "last_name", "site"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"],
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
+            site=validated_data.get("site", None),
+        )
+        return user
